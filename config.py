56c301518a0333dfe8da8fc0999d5d321a60f77a
Mounir Houl <100350646+munior24@users.noreply.github.com>
2023-02-19 19:06:18 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
-client_id = 'fc80f9431c0644e4a6033b30c84867bd'
-client_secret = '2ed8d5b0d285430996284424936f658a'
+client_id = '<your spotify api client_id>'
+client_secret = '<your spotify api client_secret>'
 
 
-postgres_connector = {}
+postgres_connector = {"host" : "<database host>",
+                      "port" : "<port>", 
+                      "database" : "<database name>",
+                      "user" : "<username>",
+                      "passwird" : "<you database password>"
+                     }
 
 
